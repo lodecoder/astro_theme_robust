efff648807b32beb7e458f3364d0d17544c637fd
lodecoder <74097794+lodecoder@users.noreply.github.com>
2025-05-13 03:07:43 +0900

    convert to astro theme

---
+++ astro_theme_robust/astro.config.mjs
@@ -0,0 +1,13 @@
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import { defineConfig } from "astro/config"
+
+export default defineConfig({
+	site: "https://example.com",
+	integrations: [mdx(), sitemap()]
+	// markdown: {
+	// 	shikiConfig: { theme: "dracula", wrap: true },
+	// 	remarkPlugins: [],
+	// 	rehypePlugins: []
+	// }
+})
